42a8bcd8c8db036d4d1af3c073123b652dac4b7d
Kyle Richardson <s.k.richardson7@gmail.com>
2015-10-01 23:00:14 -0400

    fixed typos

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return new Ember.RSVP.Promise(function(resolve, reject){
+    return new Ember.RSVP.Promise(function(resolve){
       navigator.geolocation.getCurrentPosition(function(position){
           resolve({
             latitude: position.coords.latitude,
--- app/routes/search.js
+++ app/routes/search.js
@@ -5,6 +5,6 @@ export default Ember.Route.extend({
 		console.log(params);
 		return Ember.$.ajax('https://maps.googleapis.com/maps/api/geocode/json?address='+params.locale+'&components=locality&components=postal_code&key=AIzaSyBjwLoZ2T4_wmsxFwyNA3XWUfNdFjUi1PY').then(function(response) {
 			console.log(response);
-		})
+		});
 	}
 });
